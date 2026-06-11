c5362b29330bb1cddbd839c7b53dd157809cbfbf
Vida Nava <27vnava@cpsd.us>
2026-06-11 11:06:09 -0400

    linked pages

--- js/01_var.js
+++ js/01_var.js
@@ -7,7 +7,7 @@ var x = 5;
 console.log(x);
 
 // 3. Redeclare x using var and assign it the value 2 
-var x = 2;
+
 
 // 4. Use a console.log statement to log the value of x to the console.
-console.log(x);
+
